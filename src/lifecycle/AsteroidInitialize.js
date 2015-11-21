bed26148bda148dffbfd188cc0463a4b2976a50a
typhonrt <support@typhonrt.org>
2015-11-20 22:46:15 -0800

    Initial commit

--- src/lifecycle/AsteroidInitialize.js
+++ src/lifecycle/AsteroidInitialize.js
@@ -12,4 +12,7 @@ if (typeof asteroidconfig !== object || typeof asteroidconfig.host !== string)
    throw new Error("AsteroidInitialize - `asteroidconfig` is not an object hash or doesn't contain a `host` entry.");
 }
 
+/**
+ * Export an instance of Asteroid.
+ */
 export default new Asteroid(asteroidconfig.host, asteroidconfig.ssl || false);
