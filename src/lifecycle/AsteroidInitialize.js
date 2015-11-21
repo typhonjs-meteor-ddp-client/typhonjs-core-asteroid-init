4a6e3e999595828f66adf27e5454d599948a82a0
typhonrt <support@typhonrt.org>
2015-11-20 22:39:08 -0800

    Initial commit

--- src/lifecycle/AsteroidInitialize.js
+++ src/lifecycle/AsteroidInitialize.js
@@ -7,4 +7,7 @@
 import Asteroid         from 'asteroid-browser';
 import asteroidconfig   from 'asteroidconfig';
 
+console.log("AndroidInitialize - Asteroid: " +Asteroid);
+console.log("AndroidInitialize - asteroidconfig: " +JSON.stringify(asteroidconfig));
+
 export default new Asteroid(asteroidconfig.host, asteroidconfig.ssl || false);
