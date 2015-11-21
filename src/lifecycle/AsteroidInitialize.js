f386fbad525574ca7942fc92546511316828a060
typhonrt <support@typhonrt.org>
2015-11-20 22:46:53 -0800

    Initial commit

--- src/lifecycle/AsteroidInitialize.js
+++ src/lifecycle/AsteroidInitialize.js
@@ -7,7 +7,7 @@
 import Asteroid         from 'asteroid-browser';
 import asteroidconfig   from 'asteroidconfig';
 
-if (typeof asteroidconfig !== object || typeof asteroidconfig.host !== string)
+if (typeof asteroidconfig !== 'object' || typeof asteroidconfig.host !== 'string')
 {
    throw new Error("AsteroidInitialize - `asteroidconfig` is not an object hash or doesn't contain a `host` entry.");
 }
